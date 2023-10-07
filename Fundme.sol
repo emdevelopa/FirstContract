660e414ae8d6223a0dff660e15edf3c063ca8d12
emdevelopa <gyimahsunday@gmail.com>
2023-10-07 23:07:50 +0100

    Nice

--- Fundme.sol
+++ Fundme.sol
@@ -2,14 +2,10 @@
 pragma solidity ^0.8.8;
 
 contract FundMe{
-    
-    uint256 public ethvalue;
-
     uint256 public minUSD = 50;
 
     function fundme() public payable {
 
-        require(msg.value >= minUSD, "Send more than 1 eth");
-        ethvalue = msg.value / 1e18;
+        require(msg.value / 1e18 >= minUSD, "Send more than 1 eth");
     }
 }
