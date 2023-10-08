53986bfe231f1aaf27772d41ec9db6431d7ddce1
emdevelopa <gyimahsunday@gmail.com>
2023-10-09 00:28:49 +0100

    Nice

--- Fundme.sol
+++ Fundme.sol
@@ -7,11 +7,39 @@ contract FundMe{
 
     using priceConverter for uint256;
 
-    uint256 public minUSD = 50;
+    uint256 public minUSD = 50 * 1e18;
+
+    address[] public funders;
+
+    mapping(address => uint256) public addressToAmountFund;
+
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    }
 
     function fundme() public payable {
 
         require(msg.value.getConvertionRate() >= minUSD, "Send more than 1 eth");
+        funders.push(msg.sender);
+        addressToAmountFund[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFund[funder] = 0;
+        }
+
+        funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner,"The sender is not the owner");
+        _;
     }
 
    
