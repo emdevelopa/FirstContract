b1c6dcc1c688512613f4f824c36abff7b8feca68
emdevelopa <gyimahsunday@gmail.com>
2023-10-08 04:15:15 +0100

    Nice

--- Fundme.sol
+++ Fundme.sol
@@ -1,29 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import "./priceConverter.sol";
 
 contract FundMe{
+
+    using priceConverter for uint256;
+
     uint256 public minUSD = 50;
 
     function fundme() public payable {
 
-        require(getConvertionRate(msg.value) >= minUSD, "Send more than 1 eth");
+        require(msg.value.getConvertionRate() >= minUSD, "Send more than 1 eth");
     }
 
-    // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-    function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed =AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-
-        (,int256 answer,,,)=priceFeed.latestRoundData();
-
-        return uint256(answer * 1e10);
-        
-    }
-
-    function getConvertionRate(uint256 ethAmmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-
-        uint256 ethAmountInUSD = (ethPrice * ethAmmount) / 1e18;
-        return ethAmountInUSD; 
-    }
+   
 }
+
